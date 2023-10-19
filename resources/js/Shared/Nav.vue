e9d91d4c73013681b28cd06aa976f8a08fe2c0ba
Roei Boribor <roeiboribor@gmail.com>
2023-10-19 17:02:15 +0800

    add button

--- resources/js/Shared/Nav.vue
+++ resources/js/Shared/Nav.vue
@@ -4,8 +4,12 @@
             <li><Link href="/">Home</Link></li>
             <li><Link href="/users">Users</Link></li>
             <li><Link href="/settings">Settings</Link></li>
+            <li>
+                <Link href="/logout" method="post" as="button">Logout</Link>
+            </li>
         </ul>
     </nav>
+    <!-- https://www.youtube.com/watch?v=aNZvFb402-U&list=PL3VM-unCzF8jeu0m8pSz6-Q9TwV74AbvW&index=6 -->
 </template>
 
 <script>
--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,7 @@ Route::get('/settings', function () {
 Route::get('/users', function () {
     return inertia('Users');
 });
+
+Route::post('/logout', function () {
+    return dd('Logout');
+});
